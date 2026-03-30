9495442430089715c37f1074f74e633bbfb114e3
AMANDA Info <info@amanda-technology.ch>
2026-03-30 17:14:06 +0200

    apply resharper code style

--- src/CashCtrlApiNet.AspNetCore/CashCtrlServiceCollectionExtensions.cs
+++ src/CashCtrlApiNet.AspNetCore/CashCtrlServiceCollectionExtensions.cs
@@ -23,7 +23,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-using System.Net.Http.Headers;
 using System.Text;
 using CashCtrlApiNet.Interfaces;
 using CashCtrlApiNet.Interfaces.Connectors;
